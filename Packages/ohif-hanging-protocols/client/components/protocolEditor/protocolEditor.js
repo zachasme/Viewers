77967bf498fadc5c49ee600308513b1d52acb940
Zacharias <zachasme@gmail.com>
2017-06-20 14:20:21 +0200

    try using real metadata

--- Packages/ohif-hanging-protocols/client/components/protocolEditor/protocolEditor.js
+++ Packages/ohif-hanging-protocols/client/components/protocolEditor/protocolEditor.js
@@ -5,7 +5,7 @@ import { Random } from 'meteor/random';
 import { $ } from 'meteor/jquery';
 
 import { OHIF } from 'meteor/ohif:core';
-import 'meteor/ohif:viewerbase';
+import { Viewerbase } from 'meteor/ohif:viewerbase';
 
 
 
@@ -51,13 +51,31 @@ Template.protocolEditor.events({
     cornerstoneTools.regionsThreshold.setConfiguration(config)
   },
   'click #calculate': function(event, context){
+
+    // Create the object for the instance metadata
+    let instance;
+
+    OHIF.viewer.StudyMetadataList.find(studyMetadata => {
+        // Search for the instance that has the current imageId
+        instance = studyMetadata.findInstance(instance => {
+            return true; //instance.getImageId() === imageId;
+        });
+
+        // If instance if found stop the search
+        return !!instance;
+    });
+
+    const data = instance._data;
+
     const attributes = {
-      SliceThickness: 3,
-      PixelSpacing: [0.6, 0.6],
-      KVP: 120,
-      RescaleSlope: 1,
-      RescaleIntercept: 1024,
+      SliceThickness: data.sliceThickness,
+      PixelSpacing: data.pixelSpacing.split('\\').map(parseFloat),
+      KVP: data.KPV,
+      RescaleSlope: data.RescaleSlope,
+      RescaleIntercept: data.RescaleIntercept,
     };
+    console.log("scoring using these attributes: ", attributes)
+
     cornerstoneTools.regionsScore(attributes).then(scores => {
       lastScores = scores
       console.log(lastScores)
